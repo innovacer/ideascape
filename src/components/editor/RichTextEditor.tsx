f03a43bd7c9f6d594dd9acf3620285da93939e80
innovacer <115035396+innovacer@users.noreply.github.com>
2025-04-12 12:12:55 +0100

    Update RichTextEditor.tsx

--- src/components/editor/RichTextEditor.tsx
+++ src/components/editor/RichTextEditor.tsx
@@ -1,21 +1,23 @@
+// src/components/editor/RichTextEditor.tsx
 import dynamic from 'next/dynamic';
 const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
 import 'react-quill/dist/quill.snow.css';
 
 export default function RichTextEditor({ value, onChange }) {
   return (
-    <ReactQuill 
-      theme="snow" 
-      value={value} 
+    <ReactQuill
+      theme="snow"
+      value={value}
       onChange={onChange}
+      placeholder="Start writing your idea..."
       modules={{
         toolbar: [
-          [{ 'header': [1, 2, 3, false] }],
-          ['bold', 'italic', 'underline', 'strike', 'blockquote', 'code-block'],
+          [{ 'header': [1, 2, false] }],
+          ['bold', 'italic', 'underline'],
           [{ 'list': 'ordered'}, { 'list': 'bullet' }],
           ['link', 'image'],
           ['clean']
-        ],
+        ]
       }}
     />
   );
