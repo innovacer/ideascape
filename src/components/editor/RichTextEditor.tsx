1297c09b689c809e62b8f644496f70456f0e59b0
innovacer <115035396+innovacer@users.noreply.github.com>
2025-04-15 16:46:12 +0100

    Update RichTextEditor.tsx

--- src/components/editor/RichTextEditor.tsx
+++ src/components/editor/RichTextEditor.tsx
@@ -1,6 +1,7 @@
 import dynamic from 'next/dynamic';
-const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
 import 'react-quill/dist/quill.snow.css';
+
+const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
 
 interface RichTextEditorProps {
   value: string;
